6a2e91439ffda49b90b368748b52617aa8714039
AJAskey <ajaskey34@gmail.com>
2015-11-22 09:39:49 -0700

    Created AppTemplate

--- src/net/ajaskey/market/ta/apps/AppTemplate.java
+++ src/net/ajaskey/market/ta/apps/AppTemplate.java
@@ -75,12 +75,12 @@ public class AppTemplate {
 		 */
 
 		/**
-		 * Set valid tickers for processing. Ignores data from other tickers.
+		 * Set valid tickers for processing. Other ticker data will be ignored.
 		 * 
 		 * Setting the first ticker to "PROCESS_ALL_TICKERS" will retrieve data for
 		 * all tickers.
 		 * 
-		 * Can use a ticker list file or individual ticker symbols.
+		 * You can use a ticker list file or individual ticker symbols.
 		 */
 		ParseData.clearValidTickers();
 		ParseData.setValidTickers(ParseData.getTickerList("lists\\sp500-list.txt"));
@@ -95,20 +95,38 @@ public class AppTemplate {
 		tdList.clear();
 		tdList = ParseData.parseFiles(filenames); // read all data
 		/**
-		 * or use -- tdList = ParseData.parseFiles(filenames,100); // read 100
-		 * calendar days of data. Probably will get only 100*5/7 trading days.
+		 * or request calendar days of data. Probably will get only 100*5/7 trading
+		 * days. Example: 30 calendar days = 21 trading days. This is important for
+		 * calculations. Make sure you read enough data.
+		 * 
+		 * Example: ParseData.parseFiles(filenames, 50) will not return enough data
+		 * the calculate a 50 day moving average.
 		 */
+		tdList = ParseData.parseFiles(filenames, 100);
 
 		/**
 		 * Or read only one file in the format of
 		 * 
-		 * FANG Date,Open,High,Low,Close,Volume
+		 * <code>
+		 * FANG 
+		 * Date,Open,High,Low,Close,Volume
 		 * 19-Nov-2015,80.42,81.00,76.05,77.06,1124500
 		 * 18-Nov-2015,80.00,81.86,78.11,81.42,1429500
 		 * 17-Nov-2015,80.00,80.99,78.10,79.61,842500
+		 * </code>
 		 * 
+		 * The "DumpTickerData" app will create files of this format from eoddata.
+		 * You can also get data from Yahoo or Stockcharts for individual tickers.
+		 * You can download the last 30 days of free data from www.eoddata.com and
+		 * use it to test this example app.
+		 * 
+		 * The time order of the data does not matter as the SortDailyData class can
+		 * be used to sort to the order needed for algorithms in this project.
+		 * 
+		 * A few files have been committed to use as examples.
 		 */
 		TickerData tdOne = ParseData.parseOneFile("ptv-data\\FANG.txt");
+		System.out.println(tdOne.getTicker());
 
 		/**
 		 * Or read all the valid ticker files in a directory of the ticker per file
@@ -130,8 +148,9 @@ public class AppTemplate {
 		 * Or retrieve individual ticker data.
 		 */
 		TickerData fang = TickerData.getTickerData(tdList, "FANG");
-		System.out.println(fang.getTicker());
-
+		if (fang != null) {
+			System.out.println(fang.getTicker());
+		}
 	}
 
 }
