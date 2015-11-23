c895f0185067ab2a19a2e22b53a08c5fb9c782cd
AJAskey <ajaskey34@gmail.com>
2015-11-23 10:17:49 -0700

    Updated UtilMethods

--- Test/net/ajaskey/market/ta/methods/MovingAverageMethodsTest.java
+++ Test/net/ajaskey/market/ta/methods/MovingAverageMethodsTest.java
@@ -42,7 +42,6 @@ public class MovingAverageMethodsTest {
 
 	private final TickerData	tdSC;
 	private final TickerData	td;
-	private double [] dData10 = {1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0};
 
 	public MovingAverageMethodsTest() throws FileNotFoundException, IOException, ParseException {
 
@@ -55,116 +54,22 @@ public class MovingAverageMethodsTest {
 
 	@Test
 	public void testEma10() {
-		final double ema10 = MovingAverageMethods.ema(this.tdSC, 10);
+		final double ema10 = MovingAverageMethods.ema(this.tdSC.getCloseData(), 10);
 		// System.out.println(ema10);
 		Assert.assertEquals(ema10, 22.92, 0.1);
 	}
 
 	@Test
 	public void testEma10a() {
-		final double ema10 = MovingAverageMethods.ema(this.td, 10);
+		final double ema10 = MovingAverageMethods.ema(this.td.getCloseData(), 10);
 		// System.out.println(ema10);
 		Assert.assertEquals(ema10, 108.51, 0.1);
 	}
 
 	@Test
 	public void testEma25() {
-		final double ema25 = MovingAverageMethods.ema(this.td, 25);
+		final double ema25 = MovingAverageMethods.ema(this.td.getCloseData(), 25);
 		System.out.println(ema25);
 		Assert.assertEquals(ema25, 106.76, 0.1);
-	}
-
-	@Test
-	public void testSma10() {
-		final double ma = MovingAverageMethods.sma(dData10, 10);
-		Assert.assertEquals(ma, 5.5, 0.1);
-	}
-
-	@Test
-	public void testSma10back20() {
-		final double ma = MovingAverageMethods.sma(this.tdSC, 10, 20);
-		Assert.assertEquals(ma, 22.22, 0.1);
-	}
-
-	@Test
-	public void testSma260() {
-		final double ma = MovingAverageMethods.sma(this.td.getCloseData(), 260);
-		Assert.assertEquals(ma, 105.41, 0.1);
-	}
-
-	@Test
-	public void testSma260offset() {
-		final double ma = MovingAverageMethods.sma(this.td.getCloseData(), 260, 100);
-		Assert.assertEquals(ma, 100.49, 0.1);
-	}
-
-	@Test
-	public void testSma65() {
-		final double ma = MovingAverageMethods.sma(this.td.getCloseData(), 65);
-		Assert.assertEquals(ma, 106.52, 0.1);
-	}
-
-	@Test
-	public void testSma65offset() {
-		final double ma = MovingAverageMethods.sma(this.td.getCloseData(), 65, 5);
-		Assert.assertEquals(ma, 106.79, 0.1);
-	}
-
-	@Test
-	public void testSmaErr1() {
-		final double ma = MovingAverageMethods.sma(this.tdSC, 40);
-		Assert.assertEquals(ma, 0.0, 0.1);
-	}
-
-	@Test
-	public void testSmaErr2() {
-		final double ma = MovingAverageMethods.sma(this.td, 500, 10);
-		Assert.assertEquals(ma, 0.0, 0.1);
-	}
-
-	@Test
-	public void testSmaErr3() {
-		final double ma = MovingAverageMethods.sma(this.td, 20, 1000);
-		Assert.assertEquals(ma, 0.0, 0.1);
-	}
-
-	@Test
-	public void testSmaErr4() {
-		final double ma = MovingAverageMethods.sma(this.td, -20, 10);
-		Assert.assertEquals(ma, 0.0, 0.1);
-	}
-
-	@Test
-	public void testSmaErr5() {
-		final double ma = MovingAverageMethods.sma(this.td, 20, -10);
-		Assert.assertEquals(ma, 0.0, 0.1);
-	}
-
-	@Test
-	public void testSmaErr6() {
-		final TickerData nullTd = null;
-		final double ma = MovingAverageMethods.sma(nullTd, 20, 10);
-		Assert.assertEquals(ma, 0.0, 0.1);
-	}
-
-	@Test
-	public void testSmaErr7() {
-		final TickerData nullTd = null;
-		final double ma = MovingAverageMethods.sma(nullTd, 20);
-		Assert.assertEquals(ma, 0.0, 0.1);
-	}
-
-	@Test
-	public void testSmaErr8() {
-		final double[] nullTd = null;
-		final double ma = MovingAverageMethods.sma(nullTd, 20, 10);
-		Assert.assertEquals(ma, 0.0, 0.1);
-	}
-
-	@Test
-	public void testSmaErr9() {
-		final double[] nullTd = null;
-		final double ma = MovingAverageMethods.sma(nullTd, 20);
-		Assert.assertEquals(ma, 0.0, 0.1);
 	}
 }
