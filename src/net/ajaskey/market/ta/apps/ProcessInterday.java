adf4af9db3754bcc61aa6b8b046a62eb73d89fb4
AJAskey <ajaskey34@gmail.com>
2015-11-30 14:40:57 -0700

    update interday data processing

--- src/net/ajaskey/market/ta/apps/ProcessInterday.java
+++ src/net/ajaskey/market/ta/apps/ProcessInterday.java
@@ -4,10 +4,12 @@ package net.ajaskey.market.ta.apps;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.ParseException;
 import java.util.List;
 
 import net.ajaskey.market.ta.InterdayData;
+import net.ajaskey.market.ta.Utils;
 import net.ajaskey.market.ta.input.ParseData;
 
 /**
@@ -56,60 +58,86 @@ public class ProcessInterday {
 		final String arg = "InterdayDataPath";
 		final String idDataPath = System.getProperty(arg, "");
 
-		final File f = new File(idDataPath + "\\NASDAQ_20151127.csv");
+		final File nazFile = new File(idDataPath + "\\NASDAQ_20151116.csv");
+		final List<InterdayData> idList = ParseData.parseInterdayFile(nazFile);
 
-		final List<InterdayData> idList = ParseData.parseInterdayFile(f);
+		final File nyseFile = new File(idDataPath + "\\NYSE_20151116.csv");
+		final List<InterdayData> nyseList = ParseData.parseInterdayFile(nyseFile);
+		
+		final File amexFile = new File(idDataPath + "\\AMEX_20151116.csv");
+		final List<InterdayData> amexList = ParseData.parseInterdayFile(amexFile);
 
-		int totUp = 0;
-		int totDown = 0;
-		double totFUp = 0;
-		double totFDown = 0;
-		int totPosDiv = 0;
-		int totNegDiv = 0;
-		int totBottom5th = 0;
-		int totTop5th = 0;
-		for (final InterdayData id : idList) {
-			final int diff = id.getSumUp() - id.getSumDown();
-			final long fDiff = (long) (id.getSumForceUp() - id.getSumForceDown());
-			if ((diff < -10) && (fDiff > 1000)) {
-				totPosDiv++;
-			} else if ((diff > 10) && (fDiff < -1000)) {
-				totNegDiv++;
-			}
-			final double rng = id.getDayHigh() - id.getDayLow();
-			double priceInRng = 0;
-			if (rng > 0.0) {
-				priceInRng = (id.getDayClose() - id.getDayLow()) / rng;
-			}
+		idList.addAll(amexList);
 
-			totUp += id.getSumUp();
-			totDown += id.getSumDown();
-			totFUp += id.getSumForceUp();
-			totFDown += id.getSumForceDown();
+		Utils.makeDir("out");
+		try (PrintWriter pw = new PrintWriter("out\\interday.out")) {
+			pw.printf("Ticker\tTicks\tUp\tDown\tDiff\tfUp\tfDown\tForce\tVol\tFV\tOpen\tHigh\tLow\tClose\tRng\tpRng\tcRatio%n");
 
-			System.out.printf("%s %d %d %d      %d %d %d      %d %.2f %.2f  %.2f   %.2f %.2f  %.2f%n", id.getTicker(),
-			    id.getSumUp(), id.getSumDown(), diff, (long) id.getSumForceUp(), (long) id.getSumForceDown(), fDiff,
-			    id.getSumVol(), id.getDayLow(), id.getDayHigh(), rng, id.getDayOpen(), id.getDayClose(), priceInRng);
+			int totUp = 0;
+			int totDown = 0;
+			double totFUp = 0;
+			double totFDown = 0;
+			int totPosDiv = 0;
+			int totNegDiv = 0;
+			int totBottom5th = 0;
+			int totTop5th = 0;
+			int knt = 0;
+			
+			for (final InterdayData id : idList) {
 
-			if ((id.getDayHigh() > 6.99) && (Math.abs(rng) > 0.10)) {
-				if (priceInRng > 0.80) {
-					totTop5th++;
-				} else if (priceInRng < 0.20) {
-					totBottom5th++;
+				 double rng = id.getDayHigh() - id.getDayLow();
+				
+				if ((id.getDayHigh() > 4.99) && (Math.abs(rng) > 0.10) && (id.getSumVol() > 500000) && (id.getUpdates() > 290)) {
+
+					knt++;
+
+					final int diff = id.getSumUp() - id.getSumDown();
+					final long fDiff = (long) (id.getSumForceUp() - id.getSumForceDown());
+
+					if ((diff < -10) && (fDiff > 1000)) {
+						totPosDiv++;
+					} else if ((diff > 10) && (fDiff < -1000)) {
+						totNegDiv++;
+					}
+
+					double priceInRng = 0;
+					if (rng > 0.0) {
+						priceInRng = (id.getDayClose() - id.getDayLow()) / rng;
+					}
+
+					totUp += id.getSumUp();
+					totDown += id.getSumDown();
+					totFUp += id.getSumForceUp();
+					totFDown += id.getSumForceDown();
+					
+					double fv = Math.abs((double)fDiff / id.getSumVol() * 100.0);
+					double rngPerc = rng / id.getDayClose();
+					
+
+					pw.printf("%s\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.4f\t%.2f%n", id.getTicker(),
+					    id.getUpdates(), id.getSumUp(), id.getSumDown(), diff, (long) id.getSumForceUp(),
+					    (long) id.getSumForceDown(), fDiff, id.getSumVol(), fv, id.getDayOpen(), id.getDayHigh(), id.getDayLow(),
+					    id.getDayClose(),rng, rngPerc, priceInRng);
+
+					if (priceInRng > 0.80) {
+						totTop5th++;
+					} else if (priceInRng < 0.20) {
+						totBottom5th++;
+					}
 				}
+
 			}
+			System.out.println(idList.size() + "   " + knt);
+			System.out.printf("%d %d %n", totNegDiv, totPosDiv);
+			System.out.printf("%d %d %n", totBottom5th, totTop5th);
+			System.out.printf("%d %d %n", (totUp - totDown), (long) (totFUp - totFDown));
+			System.out.printf("%.2f %.2f %n", ((double) (totUp - totDown) / (double) idList.size()),
+			    (totFUp - totFDown) / idList.size());
 
+			// this.priceInRng260 = (this.currentPrice - this.low260) / (this.high260
+			// -
+			// this.low260);
 		}
-		System.out.println(idList.size());
-		System.out.printf("%d %d %n", totPosDiv, totNegDiv);
-		System.out.printf("%d %d %n", totBottom5th, totTop5th);
-		System.out.printf("%d %d %n", (totUp - totDown), (long) (totFUp - totFDown));
-		System.out.printf("%.2f %.2f %n", ((double) (totUp - totDown) / (double) idList.size()),
-		    (totFUp - totFDown) / idList.size());
-
-		// this.priceInRng260 = (this.currentPrice - this.low260) / (this.high260 -
-		// this.low260);
-
 	}
 
 }
